fb66d2a66a056c08dac9a11b6dcdc12ff0bf8194
Nicolas Caicedo <desarrollo@myprocess.net>
2025-06-20 03:39:17 -0500

    fase 2

--- JS/header.js
+++ JS/header.js
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
         </a>
       </div>
 
-      
+
       <nav class="nav-icons">
         <ul class="nav-links">
           <li><a href="dashboard.html" title="Inicio"><i class="fas fa-home"></i></a></li>
           <li><a href="estado-cuenta.html" title="Estado de Cuenta"><i class="fas fa-file-alt"></i></a></li>
           <li><a href="pedidos.html" title="Pedidos"><i class="fas fa-box"></i></a></li>
           <li><a href="factura-venta.html" title="Factura Venta"><i class="fas fa-file-invoice-dollar"></i></a></li>
-          <!-- <li><a href="notificacion-abono.html" title="Notificación de Abono"><i class="fas fa-bell"></i></a></li>  -->  
+          <li><a href="notificacion-abono.html" title="Notificación de Abono"><i class="fas fa-bell"></i></a></li>  
           <li><a href="pqrs.html" title="PQRS"><i class="fas fa-envelope-open-text"></i></a></li>
         </ul>
       </nav>
